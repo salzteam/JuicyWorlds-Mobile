6f70b28f1349c55227815612468a77211904f64d
salzteam <akshalrizkigandari@gmail.com>
2022-12-11 15:59:49 +0700

    Implemented Favorite

--- src/screens/Favorite.js
+++ src/screens/Favorite.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 import styles from '../styles/Favorite';
 import IconComunity from 'react-native-vector-icons/MaterialCommunityIcons';
@@ -9,12 +9,25 @@ import {
     Image,
     ScrollView,
     Text,
-    useWindowDimensions,
+    Pressable,
   } from 'react-native'; 
-  import {useNavigation} from '@react-navigation/native';
+
+import {useNavigation} from '@react-navigation/native';
+import { useSelector } from 'react-redux';
 
 function Favorite() {
     const navigation = useNavigation()
+    const product = useSelector(state => state.product);
+
+    const costing = (price) => {
+        return (
+          "IDR " +
+          parseFloat(price)
+            .toFixed()
+            .replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1.")
+        );
+      };
+
   return (
     <View style={{flex: 1}}>
         <View style={styles.navbar}>
@@ -25,161 +38,81 @@ function Favorite() {
             <View>
                 <Text style={styles.category}>Food</Text>
                 <View style={styles.containerCard}>
-                    <View style={styles.card}>
-                        <Image source={Sample} style={styles.imgProduct}/>
-                        <View>
-                            <Text style={styles.titleFood}>Creamy Ice Coffee</Text>
-                            <Text style={styles.priceFood}>IDR 27.000</Text>
-                        </View>
-                    </View>
-                    <View style={styles.card}>
-                        <Image source={Sample} style={styles.imgProduct}/>
-                        <View>
-                            <Text style={styles.titleFood}>Creamy Ice Coffee</Text>
-                            <Text style={styles.priceFood}>IDR 27.000</Text>
-                        </View>
-                    </View>
-                    <View style={styles.card}>
-                        <Image source={Sample} style={styles.imgProduct}/>
-                        <View>
-                            <Text style={styles.titleFood}>Creamy Ice Coffee</Text>
-                            <Text style={styles.priceFood}>IDR 27.000</Text>
-                        </View>
-                    </View>
-                    <View style={styles.card}>
-                        <Image source={Sample} style={styles.imgProduct}/>
-                        <View>
-                            <Text style={styles.titleFood}>Creamy Ice Coffee</Text>
-                            <Text style={styles.priceFood}>IDR 27.000</Text>
-                        </View>
-                    </View>
-                    <View style={styles.card}>
-                        <Image source={Sample} style={styles.imgProduct}/>
-                        <View>
-                            <Text style={styles.titleFood}>Creamy Ice Coffee</Text>
-                            <Text style={styles.priceFood}>IDR 27.000</Text>
-                        </View>
-                    </View>
+                    {product.Favorite?.map((data)=>{
+                        if(data.category_name === "foods" && data.product_name !== "none"){
+                            return (
+                                <>
+                                    <Pressable style={styles.card} onPress={()=>{navigation.navigate("ProductDetail", data.id)}}>
+                                        <Image source={{uri: data.image}} style={styles.imgProduct}/>
+                                        <View>
+                                            <Text style={styles.titleFood}>{data.product_name}</Text>
+                                            <Text style={styles.priceFood}>{costing(data.price)}</Text>
+                                        </View>
+                                    </Pressable>
+                                </>
+                            )
+                        }
+                    })}
                 </View>
             </View>
             <View>
                 <Text style={styles.category}>Coffee</Text>
                 <View style={styles.containerCard}>
-                    <View style={styles.card}>
-                        <Image source={Sample} style={styles.imgProduct}/>
-                        <View>
-                            <Text style={styles.titleFood}>Creamy Ice Coffee</Text>
-                            <Text style={styles.priceFood}>IDR 27.000</Text>
-                        </View>
-                    </View>
-                    <View style={styles.card}>
-                        <Image source={Sample} style={styles.imgProduct}/>
-                        <View>
-                            <Text style={styles.titleFood}>Creamy Ice Coffee</Text>
-                            <Text style={styles.priceFood}>IDR 27.000</Text>
-                        </View>
-                    </View>
-                    <View style={styles.card}>
-                        <Image source={Sample} style={styles.imgProduct}/>
-                        <View>
-                            <Text style={styles.titleFood}>Creamy Ice Coffee</Text>
-                            <Text style={styles.priceFood}>IDR 27.000</Text>
-                        </View>
-                    </View>
-                    <View style={styles.card}>
-                        <Image source={Sample} style={styles.imgProduct}/>
-                        <View>
-                            <Text style={styles.titleFood}>Creamy Ice Coffee</Text>
-                            <Text style={styles.priceFood}>IDR 27.000</Text>
-                        </View>
-                    </View>
-                    <View style={styles.card}>
-                        <Image source={Sample} style={styles.imgProduct}/>
-                        <View>
-                            <Text style={styles.titleFood}>Creamy Ice Coffee</Text>
-                            <Text style={styles.priceFood}>IDR 27.000</Text>
-                        </View>
-                    </View>
+                    {product.Favorite?.map((data)=>{
+                        if(data.category_name === "coffee" && data.product_name !== "none"){
+                            return (
+                                <>
+                                    <Pressable style={styles.card} onPress={()=>{navigation.navigate("ProductDetail", data.id)}}>
+                                        <Image source={{uri: data.image}} style={styles.imgProduct}/>
+                                        <View>
+                                            <Text style={styles.titleFood}>{data.product_name}</Text>
+                                            <Text style={styles.priceFood}>{costing(data.price)}</Text>
+                                        </View>
+                                    </Pressable>
+                                </>
+                            )
+                        }
+                    })}
                 </View>
             </View>
             <View>
                 <Text style={styles.category}>Non Coffee</Text>
                 <View style={styles.containerCard}>
-                    <View style={styles.card}>
-                        <Image source={Sample} style={styles.imgProduct}/>
-                        <View>
-                            <Text style={styles.titleFood}>Creamy Ice Coffee</Text>
-                            <Text style={styles.priceFood}>IDR 27.000</Text>
-                        </View>
-                    </View>
-                    <View style={styles.card}>
-                        <Image source={Sample} style={styles.imgProduct}/>
-                        <View>
-                            <Text style={styles.titleFood}>Creamy Ice Coffee</Text>
-                            <Text style={styles.priceFood}>IDR 27.000</Text>
-                        </View>
-                    </View>
-                    <View style={styles.card}>
-                        <Image source={Sample} style={styles.imgProduct}/>
-                        <View>
-                            <Text style={styles.titleFood}>Creamy Ice Coffee</Text>
-                            <Text style={styles.priceFood}>IDR 27.000</Text>
-                        </View>
-                    </View>
-                    <View style={styles.card}>
-                        <Image source={Sample} style={styles.imgProduct}/>
-                        <View>
-                            <Text style={styles.titleFood}>Creamy Ice Coffee</Text>
-                            <Text style={styles.priceFood}>IDR 27.000</Text>
-                        </View>
-                    </View>
-                    <View style={styles.card}>
-                        <Image source={Sample} style={styles.imgProduct}/>
-                        <View>
-                            <Text style={styles.titleFood}>Creamy Ice Coffee</Text>
-                            <Text style={styles.priceFood}>IDR 27.000</Text>
-                        </View>
-                    </View>
+                    {product.Favorite?.map((data)=>{
+                        if(data.category_name === "non coffee" && data.product_name !== "none"){
+                            return (
+                                <>
+                                    <Pressable style={styles.card} onPress={()=>{navigation.navigate("ProductDetail", data.id)}}>
+                                        <Image source={{uri: data.image}} style={styles.imgProduct}/>
+                                        <View>
+                                            <Text style={styles.titleFood}>{data.product_name}</Text>
+                                            <Text style={styles.priceFood}>{costing(data.price)}</Text>
+                                        </View>
+                                    </Pressable>
+                                </>
+                            )
+                        }
+                    })}
                 </View>
             </View>
             <View>
                 <Text style={styles.category}>Add Ons</Text>
                 <View style={styles.containerCard}>
-                    <View style={styles.card}>
-                        <Image source={Sample} style={styles.imgProduct}/>
-                        <View>
-                            <Text style={styles.titleFood}>Creamy Ice Coffee</Text>
-                            <Text style={styles.priceFood}>IDR 27.000</Text>
-                        </View>
-                    </View>
-                    <View style={styles.card}>
-                        <Image source={Sample} style={styles.imgProduct}/>
-                        <View>
-                            <Text style={styles.titleFood}>Creamy Ice Coffee</Text>
-                            <Text style={styles.priceFood}>IDR 27.000</Text>
-                        </View>
-                    </View>
-                    <View style={styles.card}>
-                        <Image source={Sample} style={styles.imgProduct}/>
-                        <View>
-                            <Text style={styles.titleFood}>Creamy Ice Coffee</Text>
-                            <Text style={styles.priceFood}>IDR 27.000</Text>
-                        </View>
-                    </View>
-                    <View style={styles.card}>
-                        <Image source={Sample} style={styles.imgProduct}/>
-                        <View>
-                            <Text style={styles.titleFood}>Creamy Ice Coffee</Text>
-                            <Text style={styles.priceFood}>IDR 27.000</Text>
-                        </View>
-                    </View>
-                    <View style={styles.card}>
-                        <Image source={Sample} style={styles.imgProduct}/>
-                        <View>
-                            <Text style={styles.titleFood}>Creamy Ice Coffee</Text>
-                            <Text style={styles.priceFood}>IDR 27.000</Text>
-                        </View>
-                    </View>
+                    {product.Favorite?.map((data)=>{
+                        if(data.category_name === "addon" && data.product_name !== "none"){
+                            return (
+                                <>
+                                    <Pressable style={styles.card} onPress={()=>{navigation.navigate("ProductDetail", data.id)}}>
+                                        <Image source={{uri: data.image}} style={styles.imgProduct}/>
+                                        <View>
+                                            <Text style={styles.titleFood}>{data.product_name}</Text>
+                                            <Text style={styles.priceFood}>{costing(data.price)}</Text>
+                                        </View>
+                                    </Pressable>
+                                </>
+                            )
+                        }
+                    })}
                 </View>
             </View>
         </ScrollView>
